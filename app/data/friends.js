1c99b581440002cd93ed209fa8517bf5e2aceea3
Anthony Velardo <avelardo93@gmail.com>
2016-05-05 11:05:13 -0400

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+	"name": "Anthony Velardo"
+	"photo": "https://scontent-lga3-1.cdninstagram.com/t51.2885-19/s150x150/12568180_185399958484362_572939693_a.jpg"
+	"scores":[
+  		3,
+  		1,
+  		3,
+  		2,
+  		4,
+  		2,
+  		3,
+  		1,
+  		2,
+  		5
+  		]
+  	}
+]; 
+
+module.exports = friends;
